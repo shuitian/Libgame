a592a27f3e833856ad078925c54eb9d3960ba1b0
shuitian <a8998682@163.com>
2015-11-18 16:15:39 +0800

    利用log4net添加日志类的封装

---
+++ libgame/libgame/Log.cs
@@ -0,0 +1,20 @@
+﻿namespace UnityTool.Libgame
+{
+    public class Log
+    {
+        static log4net.ILog logger;
+        public static log4net.ILog GetInstance()
+        {
+            if (logger == null)
+            {
+                log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo("log4net.config"));
+                logger = log4net.LogManager.GetLogger(typeof(Log));
+            }
+            return logger;
+        }
+        public static void WriteLog(string msg)
+        {
+            GetInstance().Info(msg);
+        }
+    }
+}
