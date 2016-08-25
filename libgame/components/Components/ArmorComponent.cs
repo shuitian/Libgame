61577179ce2e6b9f64a3ce83310a10c77b5e54a5
shuitian <a8998682@163.com>
2016-08-25 22:52:25 +0800

    完善护甲组件，增加伤害类

--- libgame/components/Components/ArmorComponent.cs
+++ libgame/components/Components/ArmorComponent.cs
@@ -1,82 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Libgame.Characters;
+using Libgame.Damages;
 
 namespace Libgame.Components
 {
-
-    [System.Serializable]
-    public class BaseArmorList
-    {
-        public int length
-        {
-            get
-            {
-                return _baseArmorList.Length;
-            }
-        }
-
-        public float[] _baseArmorList;
-
-        public float this[int index]
-        {
-            get
-            {
-                return _baseArmorList[index];
-            }
-            set
-            {
-                _baseArmorList[index] = value;
-            }
-        }
-
-        public BaseArmorList()
-        {
-
-        }
-
-        public BaseArmorList(float[] _baseArmorList)
-        {
-            this._baseArmorList = _baseArmorList;
-        } 
-    }
-
-    [System.Serializable]
-    public class ArmorAddedValueList
-    {
-        public int length
-        {
-            get
-            {
-                return _armorAddedValueList.Length;
-            }
-        }
-
-        public float[] _armorAddedValueList;
-
-        public float this[int index]
-        {
-            get
-            {
-                return _armorAddedValueList[index];
-            }
-            set
-            {
-                _armorAddedValueList[index] = value;
-            }
-        }
-
-        public ArmorAddedValueList()
-        {
-
-        }
-
-        public ArmorAddedValueList(float[] _armorAddedValueList)
-        {
-            this._armorAddedValueList = _armorAddedValueList;
-        } 
-    }
-
     [System.Serializable]
     public class ArmorList
     {
@@ -84,11 +12,11 @@ namespace Libgame.Components
         {
             get
             {
-                return Mathf.Min(baseArmorList.length, armorAddedValueList.length);
+                return Mathf.Min(baseArmorList.Length, armorAddedValueList.Length);
             }
         }
-        public BaseArmorList baseArmorList;
-        public ArmorAddedValueList armorAddedValueList;
+        public float[] baseArmorList;
+        public float[] armorAddedValueList;
           
         public float this[int index]
         {
@@ -112,14 +40,14 @@ namespace Libgame.Components
 
         public ArmorList(int length)
         {
-            baseArmorList = new BaseArmorList(new float[length]);
-            armorAddedValueList = new ArmorAddedValueList(new float[length]);
+            baseArmorList =new float[length];
+            armorAddedValueList = new float[length];
         }
 
         public ArmorList(float[] _baseArmorList, float[] _armorAddedValueList)
         {
-            baseArmorList = new BaseArmorList(_baseArmorList);
-            armorAddedValueList = new ArmorAddedValueList(_armorAddedValueList);
+            baseArmorList = _baseArmorList;
+            armorAddedValueList = _armorAddedValueList;
         }
     }
     /// <summary>
@@ -127,48 +55,68 @@ namespace Libgame.Components
     /// </summary>
     public class ArmorComponent : CharacterComponent
     {
+        /// <summary>
+        /// 护甲类
+        /// </summary>
         public ArmorList armorList = new ArmorList();
-        //{
-        //    get
-        //    {
-                
-        //        return baseArmorList + armorAddedValueList;
-        //    }
-        //}
 
-        ///// <summary>
-        ///// 增加护甲值
-        ///// </summary>
-        ///// <param name="armorAddedValue">增加的护甲值，可负</param>
-        //public void AddArmor(float armorAddedValue)
-        //{
-        //    this.armorAddedValue += armorAddedValue;
-        //}
+        /// <summary>
+        /// 增加护甲值
+        /// </summary>
+        /// <param name="armorAddedValueList">增加的护甲值列表，可负</param>
+        public void AddArmor(float[] armorAddedValueList)
+        {
+            for(int i = 0; i < armorAddedValueList.Length; i++)
+            {
+                AddArmor(i, armorAddedValueList[i]);
+            }
+        }
 
-        ///// <summary>
-        ///// 设置护甲值
-        ///// </summary>
-        ///// <param name="baseArmor">基础护甲值</param>
-        ///// <param name="armorAddedValue">增加的护甲值</param>
-        //public void SetArmor(float baseArmor, float armorAddedValue)
-        //{
-        //    this.baseArmor = baseArmor;
-        //    this.armorAddedValue = armorAddedValue;
-        //}
+        /// <summary>
+        /// 增加单个护甲值
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="armorAddedValue"></param>
+        public void AddArmor(int index, float armorAddedValue)
+        {
+            if(index < this.armorList.armorAddedValueList.Length)
+            {
+                this.armorList.armorAddedValueList[index] += armorAddedValue;
+            }
+        }
 
-        ///// <summary>
-        ///// 最小伤害
-        ///// </summary>
-        //public float minDamage = 0;
+        /// <summary>
+        /// 设置护甲值
+        /// </summary>
+        /// <param name="baseArmorList">基础护甲值列表</param>
+        /// <param name="armorAddedValueList">增加的护甲值列表</param>
+        public void SetArmor(float[] baseArmorList, float[] armorAddedValueList)
+        {
+            this.armorList.baseArmorList = baseArmorList;
+            this.armorList.armorAddedValueList = armorAddedValueList;
+        }
 
-        ///// <summary>
-        ///// 计算护甲抵挡后的伤害,virtual
-        ///// </summary>
-        ///// <param name="damage">抵挡前的伤害</param>
-        ///// <returns>抵挡后的伤害</returns>
-        //public virtual float CalculateDamage(float damage)
-        //{
-        //    return Mathf.Max(minDamage, damage * (100F / (100 + armor)));
-        //}
+        /// <summary>
+        /// 设置护甲值
+        /// </summary>
+        /// <param name="armorList">新的护甲值类</param>
+        public void SetArmor(ArmorList armorList)
+        {
+            this.armorList = armorList;
+        }
+
+        /// <summary>
+        /// 计算护甲抵挡后的伤害,virtual
+        /// </summary>
+        /// <param name="damage">抵挡前的伤害</param>
+        /// <returns>抵挡后的伤害</returns>
+        public virtual Damage CalculateDamage(Damage damage)
+        {
+            for(int i = 0; i < damage.realDamages.Length; i++)
+            {
+                damage.realDamages[i] *= (100F) / (100 + armorList[i]);
+            }
+            return damage;
+        }
     }
 }
---
+++ libgame/components/Damages/Damage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Libgame.Characters;
+
+namespace Libgame.Damages
+{
+    public class Damage
+    {
+        public Damage()
+        {
+            isDamaged = false;
+        }
+
+        public Damage(float[] originalDamages, float[] realDamages = null, Character source = null, Character target = null, bool isDamaged = false)
+        {
+            this.source = source;
+            this.target = target;
+            this.originalDamages = originalDamages;
+            if (realDamages == null || realDamages.Length == 0)
+            {
+                this.realDamages = this.originalDamages;
+            }
+            else
+            {
+                this.realDamages = realDamages;
+            }
+            this.isDamaged = isDamaged;
+        }
+
+        /// <summary>
+        /// 伤害来源
+        /// </summary>
+        public Character source;
+
+        /// <summary>
+        /// 原始的伤害数组
+        /// </summary>
+        public float[] originalDamages;
+    
+        /// <summary>
+        /// 实际的伤害数组
+        /// </summary>
+        public float[] realDamages;
+
+        /// <summary>
+        /// 伤害目标
+        /// </summary>
+        public Character target;
+
+        /// <summary>
+        /// 是否已经造成了伤害
+        /// </summary>
+        public bool isDamaged;
+
+        /// <summary>
+        /// 实际的总伤害
+        /// </summary>
+        public float realTotalDamage;
+    }
+}
